4408dccaf64ccbcc11db08c20c32960967e41a1a
EimearCunningham <eimearcunningham@hotmail.com>
2021-03-20 09:25:11 +0000

    Wire up edit button

--- app.py
+++ app.py
@@ -107,9 +107,16 @@ def add_task():
         mongo.db.tasks.insert_one(task)
         flash("Task added!")
         return redirect(url_for("get_tasks"))
-        
+
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_tasks.html", categories=categories)
+
+
+@app.route("/edit_task/<task_id>", methods=["GET", "POST"])
+def edit_task(task_id):
+    task = mongo.db.tasks.find_one({"id": ObjectId(task_id)})
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("edit_task.html", task=task, categories=categories)
 
 
 if __name__ == "__main__":
